aa96e970b8b0166e39f5cd5f50b31ec81e8adc14
AllenCodess <Aph296@protonmail.com>
2026-02-07 16:55:33 -0500

    dom elements, event listener and functions to check if working added

---
+++ app.js
@@ -0,0 +1,13 @@
+// dom elements
+
+const adviceEl = document.querySelector(".description");
+const btnidEl = document.querySelector("#btnid");
+
+//event listener
+btnidEl.addEventListener("click", btnClicked);
+
+// function
+
+function btnClicked() {
+  console.log("btn working");
+}
