aaf269c5ec6c54265b19e920790d5ea57f5312a5
AllenCodess <Aph296@protonmail.com>
2026-02-07 17:02:32 -0500

    fetching information from url and converting it into json

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const adviceEl = document.querySelector(".description");
 const btnidEl = document.querySelector("#btnid");
 
 //event listener
-btnidEl.addEventListener("click", btnClicked);
+btnidEl.addEventListener("click", getAdvice);
 
 // function
 
-function btnClicked() {
-  console.log("btn working");
+function getAdvice() {
+  // request the information then converts it into json
+  fetch("https://api.adviceslip.com/advice").then(function (response) {
+    return response.json();
+  });
 }
