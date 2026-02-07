8f320dd005aae4e4b8091aa122a78360fb0bd8f5
AllenCodess <Aph296@protonmail.com>
2026-02-07 17:25:44 -0500

    completed the api fetching request and error handling

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ btnidEl.addEventListener("click", getAdvice);
 
 function getAdvice() {
   // request the information then converts it into json
-  fetch("https://api.adviceslip.com/advice").then(function (response) {
-    return response.json();
-  });
+  fetch("https://api.adviceslip.com/advice")
+    .then(function (response) {
+      return response.json();
+    })
+    // this will allow you to display the obect value you selected
+    .then(function (data) {
+      adviceEl.textContent = data.slip.advice;
+    })
+
+    .catch(function (error) {
+      console.error("error fetching advice: ".error);
+      adviceEl.textContent = "Oops Something Went Wrong!";
+    });
 }
